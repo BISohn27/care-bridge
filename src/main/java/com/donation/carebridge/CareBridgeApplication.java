7db5a9e052cf014d9f9f4c9c262401a912de6560
Byungil Sohn <skylord418@gmail.com>
2025-08-26 23:24:55 +0900

    chore: Gradle 프로젝트 초기 설정

---
+++ src/main/java/com/donation/carebridge/CareBridgeApplication.java
@@ -0,0 +1,13 @@
+package com.donation.carebridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareBridgeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CareBridgeApplication.class, args);
+    }
+
+}
