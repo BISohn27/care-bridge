180f12cc8f3ccdaf35db76d11406234a4f8dedff
Byungil Sohn <skylord418@gmail.com>
2025-09-07 22:50:00 +0900

    chore: aop 관련 의존성 추가

--- payment/domain/build.gradle.kts
+++ payment/domain/build.gradle.kts
@@ -3,5 +3,9 @@ dependencies {
 
     compileOnly("jakarta.persistence:jakarta.persistence-api")
     compileOnly("org.springframework.data:spring-data-jpa")
+    compileOnly("org.springframework.data:spring-data-redis")
     implementation("org.springframework:spring-core")
+
+    implementation("org.springframework:spring-aop")
+    implementation("org.aspectj:aspectjweaver")
 }
