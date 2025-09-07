e09114190d15822cd83fb11f050f28fcf648b090
Byungil Sohn <skylord418@gmail.com>
2025-09-07 22:19:36 +0900

    chore: common domain 모듈 중복 의존성 삭제

--- common/domain/build.gradle.kts
+++ common/domain/build.gradle.kts
@@ -1,5 +1,4 @@
 dependencies {
     compileOnly("jakarta.persistence:jakarta.persistence-api")
-    compileOnly("org.springframework.data:spring-data-commons")
     compileOnly("org.springframework.data:spring-data-jpa")
 }
