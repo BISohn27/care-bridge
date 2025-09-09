7dac95f88b6364edce57297264899d74211118a0
Byungil Sohn <skylord418@gmail.com>
2025-09-09 23:52:31 +0900

    feat: web-api boot 애플리케이션 임시 추가

---
+++ payment/web-api/src/main/java/com/donation/carebridge/payment/controller/PaymentController.java
@@ -0,0 +1,9 @@
+package com.donation.carebridge.payment.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/payments")
+public class PaymentController {
+
+
+}
