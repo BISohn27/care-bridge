4b6b7b5a22d7e5edce1469043d90dcfb051520e4
Byungil Sohn <skylord418@gmail.com>
2025-09-22 22:37:16 +0900

    feat: toss 결제 실행자 결제 승인 관련 임시 로직 추가

--- payment/infrastructure/src/main/java/com/donation/carebridge/payments/infrastructure/payment/executor/TossPaymentExecutor.java
+++ payment/infrastructure/src/main/java/com/donation/carebridge/payments/infrastructure/payment/executor/TossPaymentExecutor.java
@@ -1,15 +1,19 @@
 package com.donation.carebridge.payments.infrastructure.payment.executor;
 
+import com.donation.carebridge.payments.payment.dto.ConfirmPaymentCommand;
 import com.donation.carebridge.payments.payment.dto.CreatePaymentCommand;
 import com.donation.carebridge.payments.payment.dto.NextAction;
 import com.donation.carebridge.payments.payment.dto.PaymentExecutionResult;
 import com.donation.carebridge.payments.payment.dto.ProviderContext;
+import com.donation.carebridge.payments.payment.model.PaymentStatus;
 import com.donation.carebridge.payments.payment.out.PaymentExecutor;
 import com.donation.carebridge.payments.pg.model.PgProviderCode;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
 
+@Slf4j
 @Component
 public class TossPaymentExecutor implements PaymentExecutor {
 
@@ -24,7 +28,28 @@ public class TossPaymentExecutor implements PaymentExecutor {
                 new NextAction(
                     providerContext.flowType(),
                     key(),
-                    Map.of("clientId", providerContext.clientId())),
-                null);
+                    Map.of("clientId", providerContext.clientId())));
+    }
+
+    @Override
+    public PaymentExecutionResult confirmPayment(ConfirmPaymentCommand command, ProviderContext providerContext) {
+        // TODO: 실제 토스 페이먼츠 API 호출 구현 필요
+        // 현재는 스텁 구현으로 항상 성공 처리
+        log.info("Confirming payment with Toss: paymentId={}, amount={}", 
+                command.paymentId(), command.amount());
+        
+        String pgPaymentId = (String) command.providerPayload().get("paymentKey");
+        if (pgPaymentId == null) {
+            return new PaymentExecutionResult(
+                "MISSING_PAYMENT_KEY",
+                "paymentKey is required in payload"
+            );
+        }
+        
+        return new PaymentExecutionResult(
+            PaymentStatus.PAID,
+            null,
+            null
+        );
     }
 }
