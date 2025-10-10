d7e112aa9fbb92caea3b89d3adeed651e4e98cc2
Byungil Sohn <skylord418@gmail.com>
2025-10-10 15:26:05 +0900

    feat: 후원 취소, 타임아웃 만료 usecase 인터페이스 추가

---
+++ donation/domain/src/main/java/com/donation/carebridge/donation/domain/donation/application/in/DonationCanceller.java
@@ -0,0 +1,6 @@
+package com.donation.carebridge.donation.domain.donation.application.in;
+
+public interface DonationCanceller {
+
+    void cancel(String donationId);
+}
---
+++ donation/domain/src/main/java/com/donation/carebridge/donation/domain/donation/application/in/DonationExpirator.java
@@ -0,0 +1,6 @@
+package com.donation.carebridge.donation.domain.donation.application.in;
+
+public interface DonationExpirator {
+
+    void expire();
+}
