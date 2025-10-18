1dfa9cddc43537f50f1b3ef6716c1bdda112e7f4
Byungil Sohn <skylord418@gmail.com>
2025-10-18 11:30:44 +0900

    chore: common infra db 의존성 추가

--- common/infrastructure/build.gradle.kts
+++ common/infrastructure/build.gradle.kts
@@ -1,5 +1,11 @@
 dependencies {
     implementation(project(":common:common-domain"))
 
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.data:spring-data-redis")
+    implementation("org.springframework.boot:spring-boot-autoconfigure")
+
+    runtimeOnly("com.h2database:h2")
+    implementation("com.mysql:mysql-connector-j")
+    implementation("io.lettuce:lettuce-core")
 }
